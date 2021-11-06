4256c432aa29ecc908ac22a7ff5f7c582c143cff
shar27 <shariq.ahmed731@gmail.com>
2021-11-06 15:09:51 +0100

    something

--- pages/About.js
+++ pages/About.js
@@ -68,7 +68,8 @@ const theme = {
   </div>
   <section className={styles.intro}> 
   
-  <h5 className=" display-4 mt-5 mb-5 pt-4 pb-4  text-sm-center text-md-center text-lg-center">Hello, Hola, Cześć! 你好, سلام </h5>
+  <h5 className=" display-4 mt-5 mb-5 pt-4 pb-4  text-sm-center text-md-center text-lg-center">Hello, Hola, Cześć!</h5>
+  <h5 className="display-4 mt-5 mb-5 pt-4 pb-4  text-sm-center text-md-center text-lg-center">  你好, سلام   </h5>
   
   <h5 className="display-5 mb-4 mt-5 text-sm-center text-md-center text-lg-center" >My name is Shar and I am a web developer from the UK!</h5> 
   </section>
