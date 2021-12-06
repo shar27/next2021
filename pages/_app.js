e415bbf2ee0f0bb4124d58b50f90391f654d63de
shar27 <shariq.ahmed731@gmail.com>
2021-12-06 10:03:34 +0100

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.css'; 
 import '../styles/globals.css'
 import { useEffect } from "react";
-
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    import("bootstrap/dist/js/bootstrap");
-  }, []);
+  
+  import('bootstrap/dist/css/bootstrap.css');
+  
+  return (
+    <>
+  
+  <Script 
+   
+   strategy="lazyOnload"
+   src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE}`}/>
 
-  return <Component {...pageProps} />;
+<Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>  <Component {...pageProps} />
+  </>
+  )
 }
 export default MyApp
 
