827fcad1c3a3d53b053394407f8416487adb2a44
shar27 <shariq.ahmed731@gmail.com>
2022-01-19 10:46:36 +0100

    sitemap

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const withVideos = require('next-videos')
 module.exports= withVideos()
 
 module.exports = {
+ 
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./scripts/sitemap-generator");
+    }
+    return config;
+  },
+ 
   images: {
     domains: ['images.ctfassets.net'],
   
