8af824ff06c961c0c0f39da4f3bd182e9de9cbf8
shar27 <shariq.ahmed731@gmail.com>
2022-02-25 15:11:18 +0100

    no cookies

--- pages/index.js
+++ pages/index.js
@@ -100,16 +100,7 @@ export default function Home() {
         />
       </div>
           <Contact/>
-          <CookieConsent
-        location="bottom"
-        buttonText="Sure man!!"
-        cookieName="myAwesomeCookieName2"
-        style={{ background: "#2B373B" }}
-        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-        expires={150}
-      >
-        This website uses cookies to enhance the user experience.{" "}
-      </CookieConsent>
+         
      
           </div>
     
